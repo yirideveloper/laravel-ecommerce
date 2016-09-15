136b5c8b80c5c248bb68f9fd3adb92ce98d54c58
yirideveloper <topyiri1113@gmail.com>
2016-09-15 18:35:36 +1200

    Fix AdminGuest Middleware issue

--- modules/base/Mage2/Common/Middleware/RedirectIfAdminAuthenticated.php
+++ modules/base/Mage2/Common/Middleware/RedirectIfAdminAuthenticated.php
@@ -15,10 +15,10 @@ class RedirectIfAdminAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next, $guard = "admin")
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/');
+            return redirect('/admin');
         }
 
         return $next($request);
