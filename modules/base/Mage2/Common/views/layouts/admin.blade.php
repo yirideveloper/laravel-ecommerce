349d741c17e401853cbc9446f25036e5e09b93e0
yirideveloper <topyiri1113@gmail.com>
2016-09-15 13:11:12 +1200

    FixLogin Page Html Css

--- modules/base/Mage2/Common/views/layouts/admin.blade.php
+++ modules/base/Mage2/Common/views/layouts/admin.blade.php
@@ -1,39 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Mage2 Ecommerce') }}</title>
+        <title>{{ config('app.name', 'Mage2 Ecommerce') }}</title>
 
-    <!-- Styles -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
-     <!-- Scripts -->
-    <script src="{{ asset('/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('/js/materialize.min.js') }}"></script>
-    <script src="{{ asset('/js/app.js') }}"></script>
-</head>
-<body>
-<div class="container-fluid">
-    @include("layouts.admin-nav")
+        <!-- Bootstrap core CSS -->
+        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 
-    <div class="section">
-        @yield('content')
-    </div>
+        <!-- Material Design Bootstrap -->
+        <link href="{{ asset('css/mdb.css') }}" rel="stylesheet">
 
-   
-</div>
-</body>
+        <!-- Your custom styles (optional) -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = <?php
+echo json_encode([
+    'csrfToken' => csrf_token(),
+]);
+?>
+        </script>
+
+    </head>
+    <body class="fixed-sn">
+        <div class="container">
+            @include("layouts.admin-nav")
+
+            <main style="margin-top: 5rem;">
+                @yield('content')
+            </main>
+
+
+
+        </div>
+        <!-- Scripts -->
+        <!-- JQuery -->
+        <script src="{{ asset('/js/jquery.js') }}"></script>
+
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="{{ asset('js/tether.js') }}"></script>
+
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="{{ asset('js/mdb.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
