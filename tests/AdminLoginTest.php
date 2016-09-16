82580b59612ffd57bb8fbfccc24e681410b09984
yirideveloper <topyiri1113@gmail.com>
2016-09-16 19:27:25 +1200

    Fix some Select Field bug and Added few Category Test

---
+++ tests/AdminLoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\App;
+
+class AdminLoginTest extends TestCase
+{
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testGetLogin()
+    {
+        $this->assertTrue(true);
+        $this->visit('/admin/login')->see('Mage2 Admin Login');
+    }
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testLoginPost()
+    {
+        $this->assertTrue(true);
+        $this->visit('/admin/login')
+                ->type('admin@admin.com','email')
+                ->type('admin123','password')
+                ->press('Login')
+                ->seePageIs('/admin')
+                ->see("Admin");
+
+    }
+
+}
