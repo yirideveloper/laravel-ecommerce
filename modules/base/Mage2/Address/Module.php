f3d2ac36920ec2e652f0b5b2632274e6b781fb20
yirideveloper <topyiri1113@gmail.com>
2016-11-01 12:53:16 +1300

    fix the facade issue.

---
+++ modules/base/Mage2/Address/Module.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Mage2\Address;
+
+use Illuminate\Support\Facades\View;
+use Mage2\System\View\Facades\AdminConfiguration;
+use Mage2\Framework\Support\BaseModule;
+
+class Module extends BaseModule
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerAdminMenu();
+        //$this->registerAdminConfiguration();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mapWebRoutes();
+
+
+        $this->registerViewPath();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @param \Illuminate\Routing\Router $router
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        require __DIR__.'/routes.php';
+    }
+
+    protected function registerViewPath()
+    {
+        View::addLocation(__DIR__.'/views');
+    }
+
+    public function registerAdminMenu()
+    {
+
+
+        //$adminMenu = [
+        //    'label' => 'Products',
+        //    'url' => route('admin.product.index'),
+        //];
+        //AdminMenu::registerMenu($adminMenu);
+    }
+
+    public function registerAdminConfiguration()
+    {
+        $adminConfigurations[] = [
+            'title'       => 'Address Configuration',
+            'description' => 'Set Default Country for Store',
+            'edit_action' => route('admin.configuration.address'),
+        ];
+
+        foreach ($adminConfigurations as $adminConfiguration) {
+            AdminConfiguration::registerConfiguration($adminConfiguration);
+        }
+    }
+}
