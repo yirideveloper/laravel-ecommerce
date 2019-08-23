ff7e3294d30bcb8f6a813b153d64ad90de72b769
yirideveloper <topyiri1113@gmail.com>
2019-08-23 15:25:04 +1200

    variable product with specified images

--- resources/views/product/type/variable.blade.php
+++ resources/views/product/type/variable.blade.php
@@ -8,39 +8,79 @@
 <form method="post" action="{{ route('add.to.cart') }}">
     @csrf
     @php
-    $attributeIds = $product->attributes;
+    //$attributeIds = $product->attributes;
+    $attributeGroups = $product->getVariationByAttributeGroup();
     @endphp
-    @foreach ($product->attributes as $attribute)
-        <a-form-item
-            @if ($errors->has('attributes'))
-                validate-status="error"
-                help="{{ $errors->first('attributes') }}"
-            @endif
-            label="{{ $attribute->name }}">
-            
-            <a-select
-                @change="changeAttributeVariable"
-                v-decorator="[
-                    'attribute-{{ $attribute->id }}',
-                    {rules: 
-                        [
-                            {   required: true, 
-                                message: '{{ __('validation.required', ['attribute' => $attribute->name]) }}' 
-                            }
-                        ]
-                    }
-                ]">
-                @foreach ($product->attributeProductValues()->whereAttributeId($attribute->id)->get() as $attributeValue)
-                    <a-select-option
-                        key="{{ $attributeValue->id }}"
-                        data-product="{{ $attributeValue->variation }}"
-                        :value="{{ $attributeValue->id }}">
-                        {{ $attributeValue->attributeDropdownOption->display_text }}
-                    </a-select-option>           
-                @endforeach
-            </a-select>
-        </a-form-item>
+    @foreach ($attributeGroups as $attributeId => $variations)
+        <?php 
+        $attribute = $product->getAttributeById($attributeId);
+        
+        ?>
+        @if (true && $attribute->display_as === 'IMAGE')
+            <a-form-item
+                @if ($errors->has('attributes'))
+                    validate-status="error"
+                    help="{{ $errors->first('attributes') }}"
+                @endif
+                label="{{ $attribute->name }}">
+                <a-radio-group 
+                    v-decorator="[
+                        'attribute-{{ $attributeId }}',
+                        {rules: 
+                            [
+                                {   required: true, 
+                                    message: '{{ __('validation.required', ['attribute' => $attribute->name]) }}' 
+                                }
+                            ]
+                        }
+                    ]"
+                    @change="changeAttributeVariable"
+                    button-style="outline">
+                    @foreach ($variations as $variation)
+                        @php
+                            $variation->variation->images;
+                        @endphp
+                        <a-radio key="{{ $variation->id }}"
+                            value="{{ $variation->variation }}">
+                            <img style="width:25px;height:25px" src="{{ '/storage/' . $variation->attributeDropdownOption->path }}" />
+                    </a-radio>         
+                    @endforeach
+                </a-radio-group>
 
+
+            </a-form-item>
+        @else
+            <a-form-item
+                @if ($errors->has('attributes'))
+                    validate-status="error"
+                    help="{{ $errors->first('attributes') }}"
+                @endif
+                label="{{ $attribute->name }}">
+                <a-select
+                    @change="changeAttributeVariable"
+                    v-decorator="[
+                        'attribute-{{ $attributeId }}',
+                        {rules: 
+                            [
+                                {   required: true, 
+                                    message: '{{ __('validation.required', ['attribute' => $attribute->name]) }}' 
+                                }
+                            ]
+                        }
+                    ]">
+                    @foreach ($variations as $variation)
+                        @php
+                            $variation->variation->images;
+                        @endphp
+                        <a-select-option
+                            :key="{{ $variation->id }}"
+                            value="{{ $variation->variation }}">
+                            {{ $variation->attributeDropdownOption->display_text }}
+                        </a-select-option>           
+                    @endforeach
+                </a-select>
+            </a-form-item>
+        @endif
     @endforeach
     <div class="hidden-attributes">
         <input 
